0f488eae6885a9d9bc17f3495ead2696a8cb5be0
jrhii <james.r.harter.ii@gmail.com>
2019-10-25 00:27:36 -0500

    html boilerplate and updated buildfile

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://cs4500-drone-app.herokuapp.com/
 ## Dev Mode
     To run the server in dev mode, use `npm run dev`.  This will start the server with nodemon, which will restart the server on file changes
 
+## Production
+    To run in production mode, first build with `npm run build` then run with `npm run start`
--- server.src.js
+++ server.src.js
@@ -3,8 +3,8 @@ import express from 'express';
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
