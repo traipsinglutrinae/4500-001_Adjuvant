c949ed139ca6170e4c48535295efb7df70c11d3a
jrhii <james.r.harter.ii@gmail.com>
2019-10-25 00:46:48 -0500

    heroku hotfix

--- server.src.js
+++ server.src.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, '/public')))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
