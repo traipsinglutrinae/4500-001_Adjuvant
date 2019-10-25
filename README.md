ab1917f583bfc78362c02b59001bb5140ba3903e
jrhii <james.r.harter.ii@gmail.com>
2019-10-24 23:54:17 -0500

    basic express server and listener

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cs4500-drone-app
+
+## Dev Mode
+    To run the server in dev mode, use `npm run dev`.  This will start the server with nodemon, which will restart the server on file changes
--- src/server.js
+++ src/server.js
@@ -6,6 +6,5 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/web-tool', (req, res) => res.sendFile(__dirname + '/web-tool/index.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
