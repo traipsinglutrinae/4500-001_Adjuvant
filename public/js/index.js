0342aa7bb4cfa601722effd3fe1ab9666c80dd49
Jim Harter <jim@rebatebus.com>
2019-11-06 19:22:53 -0600

    moved js script to public/js

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,9 @@
+function injectMap() {
+  const map = `<div id="mapzoom" class="mapzoom" onload="mapzoom.add(this);"><div id="wrapper" unselectable="on"><div id="mapper" unselectable="on"><img id="currentMap" onLoad="mapzoom.add(this);" src="img/1.jpg" width="800" height="600" alt="" border="0"/></div>`;
+
+  $("#map").append(map);
+}
+
 function changeMap() {
   var current_image = document.getElementById("currentMap").src;
   if (current_image == "1.jpg") {
