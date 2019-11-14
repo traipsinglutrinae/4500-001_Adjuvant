eb5a9b06320e930f1939c90e36c4b5263013a034
Jim Harter <jim@rebatebus.com>
2019-11-13 19:39:59 -0600

    canvas experiment

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,18 @@
 function injectMap(imgLocation) {
-  const map = `<div id="mapzoom" class="mapzoom" onload="mapzoom.add(this);"><div id="wrapper" unselectable="on"><div id="mapper" unselectable="on"><img id="currentMap" onLoad="mapzoom.add(this);" src="${imgLocation}" width="800" height="600" alt="" border="0"/></div>`;
+  const map = `<div id="mapzoom" class="mapzoom" onload="mapzoom.add(this);"><div id="wrapper" unselectable="on"><div id="mapper" unselectable="on"><img id="currentMap" src="${imgLocation}" width="800" height="600" alt="" border="0"/></div>`;
 
-  $("#map").empty();
-  $("#map").append(map);
+  // $("#map").empty();
+  // const canvasDiv = $("#map");
+  // const canvas = $(`<canvas id="myCanvas" width="200" height="100"></canvas>`);
+  const canvas = $("#myCanvas")[0];
+  console.log(canvas);
+  const ctx = canvas.getContext("2d");
+  const img = $(
+    `<img id="currentMap" src="${imgLocation}" width="800" height="600" alt="" border="0"/>`
+  )[0];
+  ctx.drawImage(img, 10, 10);
+  // $("#map").append(canvasDiv);
+  // $("#mapzoom").focus();
 }
 
 function changeMap() {
@@ -30,7 +40,18 @@ function changeMap() {
 }
 
 document.addEventListener("DOMContentLoaded", event => {
-  injectMap("img/1.jpg");
+  // injectMap("img/1.jpg");
 });
+
+window.onload = function() {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+
+  const img = new Image(); // Image constructor
+  img.onload = () => {
+    ctx.drawImage(img, 0, 0, 800, 600);
+  };
+  img.src = "img/1.jpg";
+};
 
 let currentMap = 1;
