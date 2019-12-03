4c7e80d0ba2628cfa53e69e321e8f4bc5af1f7de
Jim Harter <jim@rebatebus.com>
2019-12-02 19:47:09 -0600

    mapupdate

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,40 @@
+const waldoConfig = [
+  {
+    src: "img/waldoPNG/w1.png",
+    x: 3400,
+    y: 2130
+  },
+  {
+    src: "img/waldoPNG/w2.png",
+    x: 3400,
+    y: 2130
+  },
+  {
+    src: "img/waldoPNG/w3.png",
+    x: 3400,
+    y: 2130
+  },
+  {
+    src: "img/waldoPNG/w4.png",
+    x: 3400,
+    y: 2130
+  },
+  {
+    src: "img/waldoPNG/w5.png",
+    x: 3400,
+    y: 2130
+  }
+];
+
 class SatMap {
-  constructor(canvas) {
+  constructor(canvas, waldoInfo) {
     this.canvas = canvas || undefined;
     this.zoomLevel = 0;
     this.xCenter = 0;
     this.yCenter = 0;
     this.stepScale = 10;
+    this.ctx = undefined;
+    this.waldo = waldoInfo;
   }
 
   get width() {
@@ -37,29 +67,35 @@ class SatMap {
   }
 
   moveRight() {
-    const maxRight = (-1 * this.width) / 2;
-    this.xCenter -= this.stepScale; //* (100 - this.zoomLevel);
+    const maxRight = -this.spareWidth / 2;
+    this.xCenter -= this.stepScale;
     this.xCenter = this.xCenter <= maxRight ? maxRight : this.xCenter;
     this.render();
   }
 
   moveLeft() {
-    const maxLeft = this.width / 2;
-    this.xCenter += this.stepScale; // * (100 - this.zoomLevel);
+    const maxLeft = this.spareWidth / 2;
+    this.xCenter += this.stepScale;
     this.xCenter = this.xCenter >= maxLeft ? maxLeft : this.xCenter;
     this.render();
   }
 
   moveUp() {
+    const maxUp = this.spareHeight / 2;
+    this.yCenter += this.stepScale;
+    this.yCenter = this.yCenter >= maxUp ? maxUp : this.yCenter;
     this.render();
   }
 
   moveDown() {
+    const maxDown = -this.spareHeight / 2;
+    this.yCenter -= this.stepScale;
+    this.yCenter = this.yCenter <= maxDown ? maxDown : this.yCenter;
     this.render();
   }
 
   render() {
-    const ctx = this.canvas.getContext("2d");
+    this.ctx = this.canvas.getContext("2d");
 
     const img = new Image(); // Image constructor
     img.onload = () => {
@@ -67,31 +103,69 @@ class SatMap {
         height: img.height / this.height,
         width: img.width / this.width
       };
-      const srcWindow = {
-        height: (100 - this.zoomLevel) * 0.01 * img.height,
-        width: (100 - this.zoomLevel) * 0.01 * img.width
-      };
-      const srcLeft = this.xCenter * windowScale.width; // + srcWindow.width;
-      const srcTop = this.yCenter * windowScale.height; // - srcWindow.height;
-      const srcRight = this.xCenter * windowScale.width + srcWindow.width;
-      const srcBottom = this.yCenter * windowScale.height + srcWindow.height;
 
-      const ratio = (srcRight - srcLeft) / (srcBottom - srcTop);
+      let srcLeft, srcTop, captureHeight, captureWidth;
+      if (img.height < img.width) {
+        const ratio = this.canvas.width / this.canvas.height;
 
-      ctx.fillStyle = "#333";
-      ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-      ctx.drawImage(
+        captureHeight =
+          this.zoomLevel === 100
+            ? 100
+            : (100 - this.zoomLevel) * 0.01 * img.height;
+        captureWidth = captureHeight * ratio;
+      } else {
+        const ratio = this.height / this.width;
+
+        captureWidth =
+          this.zoomLevel === 100
+            ? 100
+            : (100 - this.zoomLevel) * 0.01 * img.width;
+        captureHeight = captureWidth * ratio;
+      }
+
+      this.captureHeight = captureHeight;
+      this.captureWidth = captureWidth;
+      this.spareWidth = img.width - captureWidth;
+      this.spareHeight = img.height - captureHeight;
+      srcLeft = this.xCenter + Math.abs(captureWidth - img.width) / 2;
+      srcTop = this.yCenter + Math.abs(captureHeight - img.height) / 2;
+
+      this.ctx.fillStyle = "#333";
+      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(
         img,
         srcLeft,
         srcTop,
-        srcRight,
-        srcBottom,
+        this.captureWidth,
+        this.captureHeight,
         0,
         0,
         this.canvas.width,
         this.canvas.height
       );
-      // ctx.drawImage(img, 600, 450, 800, 600);
+
+      if (this.zoomLevel > 80 || 1) {
+        console.log("asdf");
+        const waldo = new Image();
+        waldo.onload = () => {
+          //draw waldo
+          this.ctx.drawImage(
+            waldo,
+            (this.waldo.x - srcLeft - this.spareWidth / 2) *
+              (1 / windowScale.width),
+            (this.waldo.y - srcTop + this.spareHeight / 2) *
+              (1 / windowScale.height),
+            25, //this.zoomLevel - 75,
+            25 //this.zoomLevel - 75
+          );
+          console.log(
+            (this.waldo.x - this.xCenter - srcLeft + this.spareWidth / 2) *
+              (1 / windowScale.width),
+            srcLeft
+          );
+        };
+        waldo.src = this.waldo.src;
+      }
     };
     img.src = "img/1.jpg";
   }
@@ -121,7 +195,7 @@ function changeMap() {
   }
 }
 
-const map = new SatMap();
+const map = new SatMap(undefined, waldoConfig[1]);
 
 document.addEventListener("DOMContentLoaded", event => {
   // injectMap("img/1.jpg");
