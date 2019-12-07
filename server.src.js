e9b8240f402c82630c858d55eb20715869c25877
traipsinglutrinae <31224956+traipsinglutrinae@users.noreply.github.com>
2019-12-07 17:57:38 -0600

    multiple versions enabled
    lots of changes

--- server.src.js
+++ server.src.js
@@ -8,4 +8,16 @@ app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')))
 
+app.get('/index', function (req, res,html) {
+ res.sendFile(path.join(__dirname+'/views/index.html'));
+});
+
+app.get('/index35', function (req, res,html) {
+ res.sendFile(path.join(__dirname+'/views/index35.html'));
+});
+
+app.get('/index68', function (req, res,html) {
+ res.sendFile(path.join(__dirname+'/views/index68.html'));
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
